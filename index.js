b2d1513938990033cb0deec234a4260df2c58287
kanamoto_shota <kanamoto_shota@Shotas-Mac-Book-Air.local>
2016-08-08 22:14:43 +0900

    pitching, fielding データも取得可能にした

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 const json2csv = require('json2csv');
+const merge = require('merge');
+const argv = require("argv");
 
 const TeamStandings = require("./lib/TeamStandings");
 const TeamStatistics = require("./lib/TeamStatistics");
 
-// config
-var year = 2015;
+// arg
+var year = argv.run().targets[0];
+if (!year) {
+    console.log("oh, year undefined !!");
+    return;
+}
 
+// var
 var tmpDir = "./tmp/";
 var destCsvFilePath = tmpDir + "Dest_" + year + ".csv";
 
@@ -18,8 +25,12 @@ var destData = [];
 allTeams.forEach(function(team) {
     var stat = new TeamStatistics(team.id, year);
     var hitting = stat.getHititng();
+    var fielding = stat.getFielding();
+    var pitching = stat.getPitching();
 
     var _d = hitting;
+    merge(_d, fielding);
+    merge(_d, pitching);
     _d.strength = team.strength;
 
     destData.push(_d);
